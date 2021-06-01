1c3b981221416fe342ab5e68f8155a92aac4e91d
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-01 15:46:01 +0000

    create random number and ask user to guess number

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /*
 
@@ -14,9 +15,15 @@ public class Game {
     Scanner input = new Scanner(System.in);
     System.out.println("What's your name?");
 
-
     String name = input.nextLine();
-    System.out.println("Welcome, " + name + "!");
+    System.out.println("Welcome, " + name + "!" );
+
+    Random rand = new Random();
+    int number = rand.nextInt(100);
+
+    System.out.println("I'm thinking of a number between 1 and 100. What do you think it is?");
+    int guess = input.nextInt();
+
   }
 
 }

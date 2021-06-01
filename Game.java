7710b29d9e91cf317c28b8e0b48d1c6a047660c4
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-01 16:01:52 +0000

    add conditional to check if number is too high or too low

--- Game.java
+++ Game.java
@@ -24,6 +24,13 @@ public class Game {
     System.out.println("I'm thinking of a number between 1 and 100. What do you think it is?");
     int guess = input.nextInt();
 
+    if (guess > number) {
+      System.out.println("Your guess was too high, try again.");
+    } else if (guess < number) {
+      System.out.println("Your guess was too low, try again.");
+    } else {
+      System.out.println("You guessed correct, " + name + "!");
+    }
   }
 
 }

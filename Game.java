51e056d0610f83a3c49c49a3168d7423b1803815
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-01 18:47:45 +0000

    add alternate solution in comments using while true

--- Game.java
+++ Game.java
@@ -24,22 +24,47 @@ public class Game {
     System.out.println("I'm thinking of a number between 1 and 100. What do you think it is?");
     
     int guess = 0;
-    int counter = 0;
+    int guesses = 0;
 
     while (guess != number){
       guess = input.nextInt();
-      counter +=1;
+      guesses +=1;
       
       if (guess > number) {
       System.out.println("Your guess was too high, try again.");
-      continue;
     } else if (guess < number) {
       System.out.println("Your guess was too low, try again.");
-      continue;
     } else {
-      System.out.println("You guessed correct, " + name + "!");
-      break;
+      System.out.println("You guessed correct, " + name + "! It only took " + guesses + " number of guesses.");
     }
     }
   }
 }
+
+
+// another way to solve this problem with setting the while loop to true
+    // while (true) {
+    //   int guess;
+    //   guess = input.nextInt();
+    //   guesses = guesses + 1;
+    //   if (guess < 1 || guess > 100) {
+    //     System.out.println(guess + " is not between 1 and 100. Please try again.");
+    //     continue;
+    //   }
+
+    //   if (guess < number) {
+    //     System.out.println("Too low. Try higher?");
+
+    //   } else if (guess > number) {
+    //     System.out.println("Too high. Try lower?");
+
+    //   } else {
+    //     System.out.println("You got it! Nice work, " + name + "!");
+    //     System.out.println("You guessed the number in " + guesses + " tries.");
+
+    //     break;
+    //   }
+    // }
+
+    // continue skips all code below and re enters the while loop
+    // break exits the while loop

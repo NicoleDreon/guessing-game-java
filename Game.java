9150e762f8f63c462ce4d943563f9d2466af0d81
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-01 15:34:04 +0000

    ask for users name and welcome

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 
 A number-guessing game.
@@ -6,7 +8,15 @@ A number-guessing game.
 
 public class Game {
   public static void main(String[] args){
+    
     System.out.println("Hello, welcome to the guessing game!");
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("What's your name?");
+
+
+    String name = input.nextLine();
+    System.out.println("Welcome, " + name + "!");
   }
 
 }
